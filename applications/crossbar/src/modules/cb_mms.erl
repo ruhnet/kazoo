dac8993262e60cde0c9b73832528869f1dd8d394
James Aimonetti <james.aimonetti@gmail.com>
2020-03-30 17:46:45 +0000

    dialyzer and formatting

--- applications/crossbar/src/modules/cb_mms.erl
+++ applications/crossbar/src/modules/cb_mms.erl
@@ -115,10 +115,10 @@ put(Context) ->
     Payload = cb_context:doc(Context),
     Ctx = case kz_im:route_type(Payload) of
               <<"onnet">> ->
-                  kz_amqp_worker:cast(Payload, fun kapi_im:publish_inbound/1),
+                  _ = kz_amqp_worker:cast(Payload, fun kapi_im:publish_inbound/1),
                   Context;
               <<"offnet">> ->
-                  kz_amqp_worker:cast(Payload, fun kapi_im:publish_outbound/1),
+                  _ = kz_amqp_worker:cast(Payload, fun kapi_im:publish_outbound/1),
                   AccountId = cb_context:account_id(Context),
                   Rate = kz_services_im:flat_rate(AccountId, 'mms', 'outbound'),
                   Charges = kz_json:from_list([{<<"charges">>, Rate}]),
